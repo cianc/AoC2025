99b8c83ce57e5e266b80f54a4a43a76ea0e8f065
Cian Cullinan <cian.cullinan@gmail.com>
2025-12-02 17:11:03 +0000

    Performance improvements

--- day02.py
+++ day02.py
@@ -1,3 +1,4 @@
+import math
 import re
 import sys
 import time
@@ -38,7 +39,44 @@ def find_complex_invalid_ids(ranges):
 
     return invalid_ids
 
-    
+def _number_of_digits(number):
+    return math.floor(math.log10(number)) + 1
+
+def generate_complex_invalid_ids(ranges):
+    '''
+    Instead of checking on all possible values in provided ranges, generate and check possible patterns.
+
+    About 10x faster than find_complex_invalid_ids() above.
+    '''
+    invalid_ids = set()
+    for start, stop in ranges:
+        min_length = _number_of_digits(start)
+        max_length = _number_of_digits(stop)
+
+        windows = set()
+        for length in range(min_length, max_length+1):
+            windows.update(set([i for i in range(1, (length // 2)+1) if length % i == 0]))
+
+        for window in windows:
+            # All numbers with `window` digits that don't start with zero.
+            candidates = (range(10**(window-1), (10**window)))
+            min_multiplier = max(2, min_length // window) # Patterns have to repeat at least once
+            max_multiplier = max_length // window
+            for candidate in candidates:
+                for multiplier in range(min_multiplier, max_multiplier+1):
+                    num_digits = _number_of_digits(candidate)
+                    power_of_10 = 10**num_digits
+                    # This is the sum of a geometric series: 1 + power_of_10 + ...
+                    # For a number c with d digits, repeating it m times is equivalent to c * (1 + 10^d + 10^(2d) + ... + 10^((m-1)d)).
+                    geometric_sum = (power_of_10**multiplier - 1) // (power_of_10 - 1)
+                    pattern = geometric_sum * candidate
+                    if pattern in range(start, stop+1):
+                        invalid_ids.add(pattern)
+
+    return invalid_ids
+
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         filename = sys.argv[1]
@@ -54,7 +92,13 @@ if __name__ == '__main__':
     complex_invalid_ids = find_complex_invalid_ids(ranges)
     end_complex = time.time()
 
+    start_generate_complex = time.time()
+    generated_complex_invalid_ids = generate_complex_invalid_ids(ranges)
+    end_generate_complex = time.time()
+
 
     print(f"part 1: {sum(simple_invalid_ids)} - time: {end_simple - start_simple:e} seconds")
     print(f"part 2: {sum(complex_invalid_ids)} - time: {end_complex - start_complex:e} seconds")
+    print(f"part 3: {sum(generated_complex_invalid_ids)} - time: {end_generate_complex - start_generate_complex:e} seconds")
+
     
