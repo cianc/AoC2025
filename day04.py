9b95bfd5830d4500109ef76dfd6fd0c757122401
Cian Cullinan <cian.cullinan@gmail.com>
2025-12-04 19:40:36 +0000

    Solution for part 2

--- day04.py
+++ day04.py
@@ -2,7 +2,10 @@ import argparse
 import itertools
 import time
 
-def parse_input(filename: str) -> tuple[list[list[str]], list[tuple[int, int]]]:
+from typing import List, Tuple
+
+
+def parse_input(filename: str) -> Tuple[List[List[str]], List[Tuple[int, int]]]:
     warehouse = []
     rolls = []
     with open(filename, 'r') as f:
@@ -18,8 +21,9 @@ def parse_input(filename: str) -> tuple[list[list[str]], list[tuple[int, int]]]:
             
     return warehouse, rolls
 
-def count_accessible_rolls(warehouse: list[str], rolls: list[int]) -> int:
+def count_and_remove_accessible_rolls(warehouse: List[str], rolls: List[int]) -> Tuple[int, List[str], List[int]]:
     accessible_roll_count = 0
+    rolls_to_remove = []
 
     for roll in rolls:
         row_index, col_index = roll
@@ -40,9 +44,13 @@ def count_accessible_rolls(warehouse: list[str], rolls: list[int]) -> int:
         
         if other_rolls_count < 4:
             accessible_roll_count += 1
+            rolls_to_remove.append(roll)
     
-    return accessible_roll_count
-
+    for roll in rolls_to_remove:
+        rolls.remove(roll)
+        warehouse[roll[0]][roll[1]] = '.'
+    
+    return accessible_roll_count, warehouse, rolls
 
 
 
@@ -54,7 +62,27 @@ if __name__ == '__main__':
     warehouse, rolls = parse_input(args.filename)
 
     part1_start_time = time.time()
-    accessible_roll_count = count_accessible_rolls(warehouse, rolls)
+    accessible_roll_count, _, _ = count_and_remove_accessible_rolls(warehouse, rolls)
     part1_end_time = time.time()
+
+    print(f"part 1 accessible rolls: {accessible_roll_count} - time taken: {part1_end_time - part1_start_time:e}")
+
+    ###################
+
+    warehouse, rolls = parse_input(args.filename)
+    original_roll_count = len(rolls)
+
+    part2_start_time = time.time()
+    previous_roll_count = original_roll_count
+    while True:
+        accessible_roll_count, warehouse, rolls = count_and_remove_accessible_rolls(warehouse, rolls)
+        # for row in warehouse:
+        #     print(''.join(row))
+        # print(f"removed rolls: {previous_roll_count - len(rolls)}")
+        # print('#' * 80)
+        if len(rolls) == previous_roll_count:
+            break
+        previous_roll_count = len(rolls)
+    part2_end_time = time.time()
     
-    print(f"accessible rolls: {accessible_roll_count} - time taken: {part1_end_time - part1_start_time:e}")
+    print(f"part 2 - removed rolls: {original_roll_count - len(rolls)} - time taken: {part2_end_time - part2_start_time:e}")
