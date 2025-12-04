dbe91c9b924528c59b181cc1c59a48f5e5b0f665
Cian Cullinan <cian.cullinan@gmail.com>
2025-12-04 20:19:27 +0000

    A few optimisations

--- day04.py
+++ day04.py
@@ -5,53 +5,58 @@ import time
 from typing import List, Tuple
 
 
+DELTAS = list(itertools.product((-1, 0, 1), repeat=2))
+DELTAS.remove((0, 0))
+
+
 def parse_input(filename: str) -> Tuple[List[List[str]], List[Tuple[int, int]]]:
     warehouse = []
     rolls = []
     with open(filename, 'r') as f:
         for row_index, line in enumerate(f.readlines()):
-            warehouse.append([])
-            for char_index, char in enumerate(line.strip()):
+            row_data = list(line.strip())
+            warehouse.append(row_data)
+            for char_index, char in enumerate(row_data):
                 if char == '@':
                     rolls.append((row_index, char_index))
-                if not warehouse[row_index]:
-                    warehouse[row_index] = [char]
-                else:
-                    warehouse[row_index].append(char)
             
     return warehouse, rolls
 
-def count_and_remove_accessible_rolls(warehouse: List[str], rolls: List[int]) -> Tuple[int, List[str], List[int]]:
-    accessible_roll_count = 0
+def count_and_remove_accessible_rolls(warehouse: List[List[str]], rolls: List[Tuple[int, int]]) -> Tuple[int, List[List[str]], List[Tuple[int, int]]]:
     rolls_to_remove = []
+    max_row_index = len(warehouse) - 1
+    if max_row_index < 0:
+        return 0, warehouse, rolls
+    num_col_index = len(warehouse[0]) - 1
 
     for roll in rolls:
         row_index, col_index = roll
-        max_row_index = len(warehouse) - 1
-        num_col_index = len(warehouse[0]) - 1
 
-        deltas = list(itertools.product((-1, 0, 1), repeat=2))
-        deltas.remove((0, 0))
         other_rolls_count = 0
-        for row_delta, col_delta in deltas:
+        for row_delta, col_delta in DELTAS:
             new_row_index = row_index + row_delta
             new_col_index = col_index + col_delta
 
-            if new_row_index < 0 or new_row_index > max_row_index or new_col_index < 0 or new_col_index > num_col_index:
+            if not (0 <= new_row_index <= max_row_index and 0 <= new_col_index <= num_col_index):
                 continue
             if warehouse[new_row_index][new_col_index] == '@':
                 other_rolls_count += 1
+                if other_rolls_count >= 4:
+                    break
         
         if other_rolls_count < 4:
-            accessible_roll_count += 1
             rolls_to_remove.append(roll)
     
-    for roll in rolls_to_remove:
-        rolls.remove(roll)
-        warehouse[roll[0]][roll[1]] = '.'
-    
-    return accessible_roll_count, warehouse, rolls
+    if not rolls_to_remove:
+        return 0, warehouse, rolls
 
+    for row_index, col_index in rolls_to_remove:
+        warehouse[row_index][col_index] = '.'
+
+    rolls_to_remove_set = set(rolls_to_remove)
+    new_rolls = [roll for roll in rolls if roll not in rolls_to_remove_set]
+    
+    return len(rolls_to_remove), warehouse, new_rolls
 
 
 if __name__ == '__main__':
