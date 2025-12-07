76cb7edc07ff2b1300df14cc7030dcb540fa6aa8
Cian Cullinan <cian.cullinan@gmail.com>
2025-12-07 17:47:39 +0000

    possible part 2 solution, but very slow

--- day07.py
+++ day07.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import time
 
 from typing import List, Tuple
 
@@ -14,7 +15,7 @@ def parse_manifold(filename: str) -> List[str]:
     return manifold
 
 
-def plot_beam(manifold: List[str]) -> Tuple[List[str], int]:
+def plot_classical_beam(manifold: List[str]) -> Tuple[List[str], int]:
     plot = copy.deepcopy(manifold)
     plot_width = len(plot[0])
     plot_depth = len(plot)
@@ -36,6 +37,53 @@ def plot_beam(manifold: List[str]) -> Tuple[List[str], int]:
 
     return plot, split_count
 
+
+def plot_quantum_beam(manifold: List[str]) -> int:
+    manifold_width = len(manifold[0])
+    manifold_depth = len(manifold)
+    #print(f"manifold_depth: {manifold_depth}")
+    #for row in manifold:
+    #    print(''.join(row))
+    #print('=================')
+
+
+    if manifold_depth == 1:
+     #   time.sleep(0.5)
+        return 1
+
+    timeline_count = 0
+
+    for c_idx, c in enumerate(manifold[0]):
+        #print(f"c_idx: {c_idx}, c: {c}")
+        if c in ('|', 'S'):
+            if manifold[1][c_idx] == '^':
+                if c_idx - 1 >= 0:
+                    manifold[1][c_idx - 1] = '|' 
+                    timeline_count += plot_quantum_beam(manifold[1:])
+                    manifold[1][c_idx - 1] = '.'
+                    #print(f"manifold_depth2: {manifold_depth} - timeline_count: {timeline_count}")
+                if c_idx + 1 < manifold_width:
+                    manifold[1][c_idx + 1] = '|'
+                    timeline_count += plot_quantum_beam(manifold[1:])
+                    manifold[1][c_idx + 1] = '.'
+                    #print(f"manifold_depth3: {manifold_depth} - timeline_count: {timeline_count}")
+                
+            else:
+                manifold[1][c_idx] = '|'
+                timeline_count += plot_quantum_beam(manifold[1:])
+                manifold[1][c_idx] = '.'
+                #print(f"manifold_depth4: {manifold_depth} - timeline_count: {timeline_count}")
+    #print(f"Finished manifold_depth: {manifold_depth} - timeline_count: {timeline_count}")
+
+
+    #time.sleep(0.5)
+    return timeline_count
+
+
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="The input file with battery banks.")
@@ -44,7 +92,17 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
+    part1_start = time.time()
     manifold = parse_manifold(args.filename)
-    beam_plot, split_count = plot_beam(manifold)
+    beam_plot, split_count = plot_classical_beam(manifold)
+    part1_end = time.time()
 
-    print(f"answer: {split_count}")
+    print(f"part1 answer: {split_count} - time: {part1_end - part1_start:e}")
+
+    #################
+
+    part2_start = time.time()
+    timeline_count = plot_quantum_beam(manifold)
+    part2_end = time.time()
+
+    print(f"part2 answer: {timeline_count} - time: {part2_end - part2_start:e}")
